5c7cfc5ad2d34a9c3fccfab70db3802ed707c652
Özgür Taylan Umucu <59730168+Taylan5567@users.noreply.github.com>
2025-08-22 17:13:57 +0200

    model Video

--- content_app/models.py
+++ content_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Video(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    thumbnail_url = models.URLField(blank=True, null=True)
+    category = models.CharField(max_length=100)
+
